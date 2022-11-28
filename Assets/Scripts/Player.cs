6cd6feb8d9548c6965e0a16fd37ea38ff711248d
badman0604 <serios0604@gmail.com>
2022-11-28 10:35:31 +0800

    Revert "新增音效"
    This reverts commit 15910e7cf9641cb969f85220ca70c3524c974355.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject gameManager;
-    public AudioClip walksound;
-    public AudioClip hurtsound;
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +17,12 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            GetComponent<AudioSource>().clip=walksound;
-            GetComponent<AudioSource>().Play();
             transform.Translate(3, 0, 0);
             //transform.position += new Vector3(3, 0, 0); 
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            GetComponent<AudioSource>().clip = hurtsound;
-            GetComponent<AudioSource>().Play();
             transform.Translate(-3, 0, 0);
         }
     }
@@ -42,14 +35,12 @@ public class Player : MonoBehaviour
     // 當玩家按下畫面左按鍵時，貓咪往左移動「3」
     public void LButtonDown()
     {
-        GetComponent<AudioSource>().Play();
         transform.Translate(-3, 0, 0);
     }
 
     // 當玩家按下畫面右按鍵時，貓咪往右移動「3」
     public void RButtonDown()
     {
-        GetComponent<AudioSource>().Play(); 
         transform.Translate(3, 0, 0);
     }
 
